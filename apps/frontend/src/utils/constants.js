d74408bcb88bc8a7fbb7b83b539135efd0add6eb
Nothan101 <notaadennn@gmail.com>
2025-01-06 14:34:29 -0600

    first commi

--- apps/frontend/src/utils/constants.js
+++ apps/frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8000/api/standgatesapi3009000800456/';
+export const API_BASE_URL = 'http://18.219.38.22:8000/api/standgatesapi3009000800456/';
 export const PAYPAL_CLIENT_ID = 'test'
-export const SERVER_URL = 'http://localhost:8000/'
+export const SERVER_URL = 'http://18.219.38.22'
